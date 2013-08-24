1c03611dc50bd88e6322f2bcc675d52d4987695b
Damian Palpacelli <damian.palpacelli@gmail.com>
2013-08-23 21:57:09 -0300

    integrado mustache para resolver vistas (se elimina el jsp resolver y renombre de .jsp a .html)

--- reparaciones/src/main/java/com/amp/controlers/HelloController.java
+++ reparaciones/src/main/java/com/amp/controlers/HelloController.java
@@ -1,27 +1,37 @@
 package com.amp.controlers;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = "/")
 public class HelloController {
 
-    protected final Log logger = LogFactory.getLog(getClass());
+	protected final Log logger = LogFactory.getLog(getClass());
 
-    @RequestMapping(value="/hello.htm")
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	@RequestMapping(method = RequestMethod.GET)
+	public String getIndex() {
+		return "index";
+	}
 
-        logger.info("Entre al controlador");
+	@RequestMapping("hello")
+	public String loadHelloPage(Model m) {
+		m.addAttribute("name", "Mustache..");
+		m.addAttribute("pagina", "partial.html");
+		return "hello";
+	}
 
-        return new ModelAndView("hello");
-    }
+	// @RequestMapping(value = "/hello.htm")
+	// public ModelAndView handleRequest(HttpServletRequest request,
+	// HttpServletResponse response) throws ServletException, IOException {
+	//
+	// logger.info("Entre al controlador");
+	//
+	// return new ModelAndView("hello");
+	// }
+
 }
